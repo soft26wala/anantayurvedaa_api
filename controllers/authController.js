af27662a8adadc0d6b4e8b10bae26126eee62110
soft26wala <soft26wabdev@gmail.com>
2026-04-10 19:27:56 +0530

    V1.0.2

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,56 +10,63 @@ import crypto from "crypto";
 import { v2 as cloudinary } from "cloudinary";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
+  const { name, email, password, number } = req.body;
+  if (!name || !email || !password || !number) {
     return next(new ErrorHandler("Please provide all required fields.", 400));
   }
-  if (password.length < 8 || password.length > 16) {
+  if (password.length < 8 || password.length > 32) {
     return next(
-      new ErrorHandler("Password must be between 8 and 16 characters.", 400)
+      new ErrorHandler("Password must be between 8 and 32 characters.", 400),
     );
   }
 
   const isAlreadyRegistered = await database.query(
     `SELECT * FROM users WHERE email = $1`,
-    [email]
+    [email],
   );
 
   if (isAlreadyRegistered.rows.length > 0) {
     return next(
-      new ErrorHandler("User already registered with this email.", 400)
+      new ErrorHandler("User already registered with this email.", 400),
     );
   }
 
-  
-  
-  
+  const isAlreadyRegisterednum = await database.query(
+    `SELECT * FROM users WHERE number = $1`,
+    [number],
+  );
+
+  if (isAlreadyRegisterednum.rows.length > 0) {
+    return next(
+      new ErrorHandler("User already registered with this number.", 400),
+    );
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
   // const hashedPassword = await bcrypt.hash(password, process.env.JWT_SECRET_KEY);
 
   const user = await database.query(
-    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
-    [name, email, hashedPassword]
+    "INSERT INTO users (name, email, password, number) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, email, hashedPassword, number],
   );
   sendToken(user.rows[0], 201, "User registered successfully", res);
 });
 
-
 export const login = catchAsyncErrors(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { number, password } = req.body;
 
-  if (!email || !password) {
-    return next(new ErrorHandler("Please provide email and password.", 400));
+  if (!number || !password) {
+    return next(new ErrorHandler("Please provide number and password.", 400));
   }
 
   // 1. यूज़र ढूंढो
   const userResult = await database.query(
-    `SELECT * FROM users WHERE email = $1`,
-    [email]
+    `SELECT * FROM users WHERE number = $1`,
+    [number],
   );
 
   if (userResult.rows.length === 0) {
-    return next(new ErrorHandler("Invalid email or password.", 401));
+    return next(new ErrorHandler("Invalid number or password.", 401));
   }
 
   const user = userResult.rows[0];
@@ -68,7 +75,7 @@ export const login = catchAsyncErrors(async (req, res, next) => {
   const isPasswordMatch = await bcrypt.compare(password, user.password);
 
   if (!isPasswordMatch) {
-    return next(new ErrorHandler("Invalid email or password.", 401));
+    return next(new ErrorHandler("Invalid number or password.", 401));
   }
 
   // 3. सफल → token भेजो
@@ -90,7 +97,7 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
       expires: new Date(Date.now()),
       httpOnly: true,
       secure: true,
-      samesite : "none"
+      samesite: "none",
     })
     .json({
       success: true,
@@ -103,7 +110,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   const { frontendUrl } = req.query;
   let userResult = await database.query(
     `SELECT * FROM users WHERE email = $1`,
-    [email]
+    [email],
   );
   if (userResult.rows.length === 0) {
     return next(new ErrorHandler("User not found with this email.", 404));
@@ -114,7 +121,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
   await database.query(
     `UPDATE users SET reset_password_token = $1, reset_password_expire = to_timestamp($2) WHERE email = $3`,
-    [hashedToken, resetPasswordExpireTime / 1000, email]
+    [hashedToken, resetPasswordExpireTime / 1000, email],
   );
 
   const resetPasswordUrl = `${frontendUrl}/password/reset/${resetToken}`;
@@ -134,7 +141,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   } catch (error) {
     await database.query(
       `UPDATE users SET reset_password_token = NULL, reset_password_expire = NULL WHERE email = $1`,
-      [email]
+      [email],
     );
     return next(new ErrorHandler("Email could not be sent.", 500));
   }
@@ -148,7 +155,7 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
     .digest("hex");
   const user = await database.query(
     "SELECT * FROM users WHERE reset_password_token = $1 AND reset_password_expire > NOW()",
-    [resetPasswordToken]
+    [resetPasswordToken],
   );
   if (user.rows.length === 0) {
     return next(new ErrorHandler("Invalid or expired reset token.", 400));
@@ -163,14 +170,14 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
     req.body.confirmPassword?.length > 16
   ) {
     return next(
-      new ErrorHandler("Password must be between 8 and 16 characters.", 400)
+      new ErrorHandler("Password must be between 8 and 16 characters.", 400),
     );
   }
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
   const updatedUser = await database.query(
     `UPDATE users SET password = $1, reset_password_token = NULL, reset_password_expire = NULL WHERE id = $2 RETURNING *`,
-    [hashedPassword, user.rows[0].id]
+    [hashedPassword, user.rows[0].id],
   );
   sendToken(updatedUser.rows[0], 200, "Password reset successfully", res);
 });
@@ -182,7 +189,7 @@ export const updatePassword = catchAsyncErrors(async (req, res, next) => {
   }
   const isPasswordMatch = await bcrypt.compare(
     currentPassword,
-    req.user.password
+    req.user.password,
   );
   if (!isPasswordMatch) {
     return next(new ErrorHandler("Current password is incorrect.", 401));
@@ -198,7 +205,7 @@ export const updatePassword = catchAsyncErrors(async (req, res, next) => {
     confirmNewPassword.length > 16
   ) {
     return next(
-      new ErrorHandler("Password must be between 8 and 16 characters.", 400)
+      new ErrorHandler("Password must be between 8 and 16 characters.", 400),
     );
   }
 
@@ -236,7 +243,7 @@ export const updateProfile = catchAsyncErrors(async (req, res, next) => {
         folder: "Ecommerce_Avatars",
         width: 150,
         crop: "scale",
-      }
+      },
     );
     avatarData = {
       public_id: newProfileImage.public_id,
@@ -248,12 +255,12 @@ export const updateProfile = catchAsyncErrors(async (req, res, next) => {
   if (Object.keys(avatarData).length === 0) {
     user = await database.query(
       "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
-      [name, email, req.user.id]
+      [name, email, req.user.id],
     );
   } else {
     user = await database.query(
       "UPDATE users SET name = $1, email = $2, avatar = $3 WHERE id = $4 RETURNING *",
-      [name, email, avatarData, req.user.id]
+      [name, email, avatarData, req.user.id],
     );
   }
 
