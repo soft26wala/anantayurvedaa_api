27790be2cc1727fae414d42b2e7c38e25a47d447
soft26wala <soft26wabdev@gmail.com>
2026-04-19 16:38:46 +0530

    v1.0.59

--- router/payment.js
+++ router/payment.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createOrder , verifyPayment } from "../controllers/payments.controllers.js";
 import dotenv from "dotenv";
+import { createCODOrder } from "../controllers/cod.js"
 dotenv.config();
 
 const router = express.Router();
@@ -9,6 +10,6 @@ const router = express.Router();
 
 router.post("/create-order", createOrder);
 router.post("/verify-payment", verifyPayment);
-
+router.post("/create-cod", createCODOrder);
 
 export default router;
