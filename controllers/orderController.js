b14a63b24b89ecbfff7f7e949b395cd4b01492cc
soft26wala <soft26wabdev@gmail.com>
2026-04-19 17:55:48 +0530

    v1.0.6 api cod fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -217,7 +217,7 @@ GROUP BY o.id, s.id
 export const fetchAllOrders = catchAsyncErrors(async (req, res, next) => {
   const result = await database.query(`
             SELECT o.*,
- COALESCE(json_agg(
+COALESCE(json_agg(
  json_build_object(
  'order_item_id', oi.id,
  'order_id', oi.order_id,
@@ -226,21 +226,28 @@ export const fetchAllOrders = catchAsyncErrors(async (req, res, next) => {
  'price', oi.price,
  'image', oi.image,
  'title', oi.title
-)
-) FILTER (WHERE oi.id IS NOT NULL), '[]' ) AS order_items, json_build_object(
-'full_name', s.full_name,
+ )
+) FILTER (WHERE oi.id IS NOT NULL), '[]') AS order_items,
+
+json_build_object(
+ 'full_name', s.full_name,
  'state', s.state,
  'city', s.city,
  'country', s.country,
  'address', s.address,
  'pincode', s.pincode,
- 'phone', s.phone 
+ 'phone', s.phone
 ) AS shipping_info
+
 FROM orders o
 LEFT JOIN order_items oi ON o.id = oi.order_id
 LEFT JOIN shipping_info s ON o.id = s.order_id
-WHERE o.paid_at IS NOT NULL
+
+-- ❌ REMOVE THIS LINE
+-- WHERE o.paid_at IS NOT NULL
+
 GROUP BY o.id, s.id
+ORDER BY o.created_at DESC;
         `);
 
   res.status(200).json({
